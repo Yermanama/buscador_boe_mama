aae9ff1d2da9ae12befe351a3050ef0b1e8d584c
Yermanama <martinezdiazgerman9@gmail.com>
2023-07-08 09:01:57 +0200

    Prqueña actualización almería

--- modules/Almeria.py
+++ modules/Almeria.py
@@ -9,9 +9,12 @@ def buscar_almeria(nombre_archivo):
     driver.get('https://www.dipalme.org/Servicios/cmsdipro/index.nsf/bop_view.xsp?p=dipalme')
     driver.find_element(by=By.CSS_SELECTOR, value='button.registerbtn.rechazarbtn.cerrar_ventana_cookies_rechazar').click()
     driver.find_element(by=By.ID, value='search-button-bop-show').click()
+    driver.implicitly_wait(5)
     driver.find_element(by=By.ID, value= 'view:form_texto').send_keys('ingeniero de caminos')
     driver.find_element(by=By.ID, value='sf_submit').click()
-    driver.find_element(by=By.ID, value='col-contenido').find_elements()
+    driver.find_element(by=By.CSS_SELECTOR, value='button.registerbtn.rechazarbtn.cerrar_ventana_cookies_rechazar').click()
+    driver.find_element(by=By.ID, value='view:_id6:repeat1').find_element(by=By.CSS_SELECTOR, value ='div.right')
+    # Me falta obtener el párrafo y la información que hay en el
 
 if __name__ == "__main__":
     buscar_almeria('prueba.txt')
